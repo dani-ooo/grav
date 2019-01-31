9fadb992a409473ec047cee329adfd2fc001ec99
Matias Griese <matias@trilbymedia.com>
2019-02-01 00:11:05 +0200

    Added search() for user collection

--- system/src/Grav/Common/User/UserCollection.php
+++ system/src/Grav/Common/User/UserCollection.php
@@ -9,8 +9,38 @@
 
 namespace Grav\Common\User;
 
+use Grav\Common\Utils;
 use Grav\Framework\Flex\FlexCollection;
 
 class UserCollection extends FlexCollection
 {
+    /**
+     * @param string $text
+     * @param bool $case_sensitive
+     * @param bool $strict
+     * @return UserCollection
+     */
+    public function search(string $text, bool $case_sensitive = false, bool $strict = false) : UserCollection
+    {
+        $text = trim($text);
+
+        if (!$text) {
+            return $this;
+        }
+
+        $matching = [];
+        /**
+         * @var string $key
+         * @var User $object
+         */
+        foreach ($this as $key => $object) {
+            if ($strict && Utils::startsWith($object->getProperty('email'), $text, $case_sensitive)) {
+                $matching[$key] = $object;
+            } elseif (!$strict && Utils::contains($object->getProperty('email'), $text, $case_sensitive)) {
+                $matching[$key] = $object;
+            }
+        }
+
+        return $this->createFrom($matching);
+    }
 }
