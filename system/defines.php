22eedbfd1160a38f24525c50cb4ca61caec4a1f8
Andy Miller <rhuk@mac.com>
2019-01-30 17:18:59 -0700

    Defines update

--- system/defines.php
+++ system/defines.php
@@ -8,7 +8,7 @@
 
 // Some standard defines
 define('GRAV', true);
-define('GRAV_VERSION', '1.6.0-beta.8');
+define('GRAV_VERSION', '1.7.0-beta.1');
 define('GRAV_TESTING', true);
 define('DS', '/');
 
